b7e674ab7726b582b27c0dc4a8a3db6f6e770f02
Raihan-Sharif <raihan.raju@gmail.com>
2022-08-31 23:30:01 +0600

    add transactions model.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_expense_app/transactions.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,18 +16,70 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  List<Transactions> myTransactionList = [
+    Transactions(
+      id: 1,
+      title: 'Buy Shirt',
+      amount: 1500,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 2,
+      title: 'Buy Book',
+      amount: 1200,
+      date: DateTime.now().add(const Duration(days: 50)),
+    ),
+    Transactions(
+      id: 3,
+      title: 'Transportation',
+      amount: 2000,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 4,
+      title: 'House Rent',
+      amount: 1200,
+      date: DateTime.now().add(const Duration(days: -5)),
+    ),
+    Transactions(
+      id: 5,
+      title: 'Food',
+      amount: 10000,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("My Expense App")),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Card(
-            child: Text("Chart"),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.amberAccent,
+              child: Text(
+                "Chart",
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
-          Card(
-            child: Text("List of text"),
-          )
+          Container(
+              width: 300,
+              child: Column(
+                children: myTransactionList.map((tx) {
+                  return Card(
+                    color: Colors.amber,
+                    child: Text(
+                      tx.title,
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }).toList(),
+              ))
         ],
       ),
     );
---
+++ lib/transactions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class Transactions {
+  final int id;
+  final String title;
+  final double amount;
+  DateTime date;
+
+  Transactions(
+      {required this.id,
+      required this.title,
+      required this.amount,
+      required this.date});
+}
