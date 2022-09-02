bab3d06db4134218485a4b353ea1f06b3f2db682
Raihan-Sharif <raihan.raju@gmail.com>
2022-09-03 01:21:23 +0600

    Widget style and TextField Widget Added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import 'package:my_expense_app/transactions.dart';
 
 void main() {
@@ -55,11 +57,12 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(title: Text("My Expense App")),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
+              elevation: 5,
               color: Colors.amberAccent,
               child: Text(
                 "Chart",
@@ -67,19 +70,83 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-              width: 300,
-              child: Column(
-                children: myTransactionList.map((tx) {
-                  return Card(
-                    color: Colors.amber,
-                    child: Text(
-                      tx.title,
-                      textAlign: TextAlign.center,
+          Card(
+            child: Column(
+              children: <Widget>[
+                TextField(decoration: InputDecoration(labelText: 'Title,')),
+                TextField(decoration: InputDecoration(labelText: 'Amount,')),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => {},
+            child: Text('Add Transactions'),
+          ),
+          Column(
+            children: myTransactionList.map((tx) {
+              return Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 5,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 5.0,
+                        offset: Offset(0.0, 2))
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 1,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '${tx.amount.toString()} BDT',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
-                  );
-                }).toList(),
-              ))
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat().add_yMMMd().format(tx.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
